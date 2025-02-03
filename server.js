e4f102111a9529e70509d431bdc490d2eb61aede
HeshamAbdallah02 <75794796+HeshamAbdallah02@users.noreply.github.com>
2025-02-03 05:11:05 +0200

    modified:   server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:3000'], // Add your frontend URL when deployed
+    origin: ['http://localhost:3000', 'https://portfolio-backend-helgxq.fly.dev'],
     credentials: true
 }));
 app.use(express.json());
